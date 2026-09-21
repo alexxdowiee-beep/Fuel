a9078ee85904cec553f18c6f66943feecb5a0094
alexxdowiee-beep <alexxdowiee@gmail.com>
2026-09-21 17:20:46 +1200

    Match Forge look and feel (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fuel-v2';
+const CACHE = 'fuel-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
